04da47066ab9d9a88a2e46f164a8f73e2154b3f9
vlad kuznetsov <vlad.y.kuznetsov@gmail.com>
2020-01-10 20:59:17 -0500

    Heroku config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('errorhandler');
 const express = require('express');
+const morgan = require('morgan');
 
 const apiRouter = require('./api');
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('tiny'));
 
 app.use('/api', apiRouter);
 
